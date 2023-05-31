ab831915dc993066750146177d7343b55601c078
udaraju matta <udarajumatta2603@gmail.com>
2023-05-31 15:47:54 +0530

    msg

---
+++ src/com/addition/AdditionOfTwoNumbers.java
@@ -0,0 +1,19 @@
+package com.addition;
+
+public class AdditionOfTwoNumbers {
+
+	public static void main(String[] args) {
+		
+			
+				int num1=10;
+				int num2=20;
+				int result=num1+num2;
+				System.out.println("Addition of two numbers "+result);
+
+			
+
+		
+
+	}
+
+}
